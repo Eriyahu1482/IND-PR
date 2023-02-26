3b389a0904e0c0f9086585dd4e934c564caec0e9
Eriyahu1482 <124336619+Eriyahu1482@users.noreply.github.com>
2023-02-26 12:48:37 +0300

    Добавьте файлы проекта.

---
+++ IND-PR/View/Pages/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static IND_PR.Core.Core;
+
+namespace IND_PR.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();          
+        }
+
+        private void Exercises_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
